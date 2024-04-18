dadcd3b6972f2c573a4fbd75ffc7fec034836453
Juan Montenegro <j.montenegroc@uniandes.edu.co>
2024-04-17 23:56:45 -0500

    front Csv
    prediction from csv working

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,79 +1,134 @@
-import * as React from 'react';
-import { alpha } from '@mui/material';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import {useState} from "react";
+import { alpha } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import Typography from "@mui/material/Typography";
 import axios from "axios";
+import Papa from "papaparse";
+import * as React from "react";
+import { useState } from "react";
 
 export default function Hero() {
+  const [isFileInput, setIsFileInput] = useState(false);
+  const [review, setReview] = useState("");
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [review, setReview] = useState('');
-    const [reviews, setReviews] = useState([]);
+  const handleReviewChange = (event) => {
+    // console.log('Review:', event.target.value);
+    setReview(event.target.value);
+  };
+  const handleFileUpload = async (event) => {
+    setIsLoading(true);
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
 
-    const handleReviewChange = (event) => {
-        // console.log('Review:', event.target.value);
-        setReview(event.target.value);
-    };
-
-    const handlePredictClick = async () => {
-        if(review === '') {
-            return;
-        }
+    Papa.parse(file, {
+      complete: async function (results) {
         try {
-            const response = await axios.post('http://localhost:8000/predict', { Review: review });
-
-            const stars = response.data.result[0];
-            const newReview = { text: review, stars: stars };
-            setReviews([...reviews, newReview]); // Add the new review to the list
-            setReview('');
-            console.log('Predicted review:', response.data.result[0]);
+          const response = await axios.post(
+            "http://localhost:8000/upload-csv",
+            formData,
+            {
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+            }
+          );
+          const predictions = response.data.predictions;
+          const newReviews = predictions.map((stars, i) => ({
+            text: results.data[i + 1][0],
+            stars: stars,
+          }));
+          setReviews([...reviews, ...newReviews]);
+          setIsLoading(false);
         } catch (error) {
-            console.error('Error predicting review:', error);
-
+          console.error("Error predicting review:", error);
         }
-    };
+      },
+    });
+    
+  };
+  const handlePredictClick = async () => {
+    if (review === "") {
+      return;
+    }
+    try {
+      const response = await axios.post("http://localhost:8000/predict", {
+        Review: review,
+      });
 
-    const renderStars = (stars) => {
-        return Array(stars).fill().map((_, i) => '⭐').join('');
-    };
+      const stars = response.data.result[0];
+      const newReview = { text: review, stars: stars };
+      setReviews([...reviews, newReview]); // Add the new review to the list
+      setReview("");
+      console.log("Predicted review:", response.data.result[0]);
+    } catch (error) {
+      console.error("Error predicting review:", error);
+    }
+  };
 
+  const handleInputTypeChange = (event, newValue) => {
+    setIsFileInput(newValue === 'file');
+  };
+
+  const renderStars = (stars) => {
+    return Array(stars)
+      .fill()
+      .map((_, i) => "⭐")
+      .join("");
+  };
 
   return (
     <Box
       id="hero"
       sx={(theme) => ({
-        width: '100%',
+        width: "100%",
         backgroundImage:
-          theme.palette.mode === 'light'
-            ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
-            : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
-        backgroundSize: '100% 20%',
-        backgroundRepeat: 'no-repeat',
+          theme.palette.mode === "light"
+            ? "linear-gradient(180deg, #CEE5FD, #FFF)"
+            : `linear-gradient(#02294F, ${alpha("#090E10", 0.0)})`,
+        backgroundSize: "100% 100px",
+        backgroundRepeat: "no-repeat",
       })}
     >
       <Container
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
           pt: { xs: 14, sm: 20 },
           pb: { xs: 8, sm: 12 },
         }}
       >
-        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+        <ToggleButtonGroup
+          value={isFileInput ? "file" : "text"}
+          exclusive
+          onChange={handleInputTypeChange}
+          aria-label="text alignment"
+        >
+          <ToggleButton value="text" aria-label="left aligned">
+            Text
+          </ToggleButton>
+          <ToggleButton value="file" aria-label="centered">
+            File
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%", sm: "70%" } }}>
           <Typography
             variant="h1"
             sx={{
-              display: 'flex',
-              flexDirection: { xs: 'column', md: 'row' },
-              alignSelf: 'center',
-              textAlign: 'center',
-              fontSize: 'clamp(3.5rem, 10vw, 4rem)',
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              alignSelf: "center",
+              textAlign: "center",
+              fontSize: "clamp(3.5rem, 10vw, 4rem)",
             }}
           >
             Try our&nbsp;
@@ -81,30 +136,47 @@ export default function Hero() {
               component="span"
               variant="h1"
               sx={{
-                fontSize: 'clamp(3rem, 10vw, 4rem)',
+                fontSize: "clamp(3rem, 10vw, 4rem)",
                 color: (theme) =>
-                  theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+                  theme.palette.mode === "light"
+                    ? "primary.main"
+                    : "primary.light",
               }}
             >
               Pipeline
             </Typography>
           </Typography>
-          <Typography
-            textAlign="center"
-            color="text.secondary"
-            sx={{ alignSelf: 'center', width: { sm: '100%', md: '80%' } }}
-          >
-            Explore our cutting-edge predictor. Elevate your experience with top-tier features
-            and services.
-          </Typography>
-          <Stack
-            direction={{ xs: 'column', sm: 'row' }}
-            alignSelf="center"
-            spacing={1}
-            useFlexGap
-            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
-          >
-            <TextField
+
+          {isFileInput  ? (
+            <Stack
+              direction={{ xs: "column", sm: "row" }}
+              alignSelf="center"
+              spacing={1}
+              useFlexGap
+              sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
+            >
+              <input
+                accept="text/csv"
+                id="contained-button-file"
+                type="file"
+                hidden
+                onChange={handleFileUpload}
+              />
+              <label htmlFor="contained-button-file">
+                <Button variant="contained" color="primary" component="span">
+                  Upload CSV
+                </Button>
+              </label>
+            </Stack>
+          ) : (
+            <Stack
+              direction={{ xs: "column", sm: "row" }}
+              alignSelf="center"
+              spacing={1}
+              useFlexGap
+              sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
+            >
+              <TextField
                 value={review}
                 onChange={handleReviewChange}
                 id="outlined-basic"
@@ -114,54 +186,63 @@ export default function Hero() {
                 aria-label="Enter a review"
                 placeholder="Your review"
                 inputProps={{
-                    autocomplete: 'off',
-                    ariaLabel: 'Enter your review',
+                  autocomplete: "off",
+                  ariaLabel: "Enter your review",
                 }}
-            />
-            <Button variant="contained" color="primary" onClick={handlePredictClick}>
-              Predict now
-            </Button>
-          </Stack>
-          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
-            By clicking &quot;Predict now&quot; you agree to our&nbsp;
-            <Link href="#" color="primary">
-              Terms & Conditions
-            </Link>
-            .
-          </Typography>
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handlePredictClick}
+              >
+                Predict now
+              </Button>
+            </Stack>
+          )}
         </Stack>
-        <Box
-          id="image"
-          sx={(theme) => ({
-            mt: { xs: 8, sm: 10 },
-            alignSelf: 'center',
-            height: { xs: 200, sm: 700 },
-            width: '100%',
-            backgroundImage:
-              theme.palette.mode === 'light'
-                ? 'url("/static/images/templates/templates-images/hero-light.png")'
-                : 'url("/static/images/templates/templates-images/hero-dark.png")',
-            backgroundSize: 'cover',
-            borderRadius: '10px',
-            outline: '1px solid',
-            outlineColor:
-              theme.palette.mode === 'light'
-                ? alpha('#BFCCD9', 0.5)
-                : alpha('#9CCCFC', 0.1),
-            boxShadow:
-              theme.palette.mode === 'light'
-                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
-                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
-          })}
-        >
+        {isLoading ? (
+          <div style={{marginTop: 50}}>
+          <CircularProgress />
+          </div>
+        ) : (
+          <Box
+            id="image"
+            sx={(theme) => ({
+              mt: { xs: 8, sm: 10 },
+              alignSelf: "center",
+              width: "100%",
+              backgroundImage:
+                theme.palette.mode === "light"
+                  ? 'url("/static/images/templates/templates-images/hero-light.png")'
+                  : 'url("/static/images/templates/templates-images/hero-dark.png")',
+              backgroundSize: "cover",
+              borderRadius: "10px",
+              outline: "1px solid",
+              outlineColor:
+                theme.palette.mode === "light"
+                  ? alpha("#BFCCD9", 0.5)
+                  : alpha("#9CCCFC", 0.1),
+              boxShadow:
+                theme.palette.mode === "light"
+                  ? `0 0 12px 8px ${alpha("#9CCCFC", 0.2)}`
+                  : `0 0 24px 12px ${alpha("#033363", 0.2)}`,
+            })}
+          >
             {reviews.map((review, index) => (
-                <div key={index} style={{ fontSize: '24px', textAlign: 'center', padding: '10px' }}>
-                    {renderStars(review.stars)} - "{review.text}"
-                    <hr/>
-                </div>
-
+              <div
+                key={index}
+                style={{
+                  fontSize: "24px",
+                  textAlign: "center",
+                  padding: "10px",
+                }}
+              >
+                {renderStars(review.stars)} - "{review.text}"
+                <hr />
+              </div>
             ))}
-        </Box>
+          </Box>
+        )}
       </Container>
     </Box>
   );
